82f95a55dfb12a74261dc4c568861525dd3f2f99
joakim lundell selander <joakim.lundellselander@elev.ga.ntig.se>
2024-09-23 09:53:19 +0200

    fighting spel

--- spel/Program.cs
+++ spel/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int heroHp = 100;
+int villainHp = 100;
+
+string heroName = "Joakim den coola";
+string villainName = "Rassiga Musen";
+
+Random generator = new Random();
+
+while (heroHp > 0 && villainHp > 0)
+{
+  Console.WriteLine("\n----- ===== NY RUNDA ===== -----");
+  Console.WriteLine($"{heroName}: {heroHp}  {villainName}: {villainHp}\n");
+
+  int heroDamage = generator.Next(20);
+  villainHp -= heroDamage;
+  villainHp = Math.Max(0, villainHp);
+  Console.WriteLine($"{heroName} gör {heroDamage} skada på {villainName}");
+
+  int villainDamage = generator.Next(20);
+  heroHp -= villainDamage;
+  heroHp = Math.Max(0, heroHp);
+  Console.WriteLine($"{villainName} gör {villainDamage} skada på {heroName}");
+
+  Console.WriteLine("Tryck på valfri knapp för att fortsätta.");
+  Console.ReadKey();
+}
+
+Console.WriteLine("\n----- ===== STRIDEN ÄR SLUT ===== -----");
+
+if (heroHp == 0 && villainHp == 0)
+{
+  Console.WriteLine("OAVGJORT");
+}
+else if (heroHp == 0)
+{
+  Console.WriteLine($"{villainName} vann!");
+}
+else
+{
+  Console.WriteLine($"{heroName} vann!");
+}
+
+Console.WriteLine("Tryck på valfri knapp för att avsluta.");
